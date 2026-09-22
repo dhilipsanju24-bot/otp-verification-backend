4184f50c5880a6b9e05b4f191f43a020f37fb697
Dhilip Sanju <dhilipsanju@Dhilips-MacBook-Air.local>
2026-09-22 15:50:59 +0530

    Use environment variables for Firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,9 +1,12 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import serviceAccount from "../firebase-service-account.json";
 
 const firebaseApp = initializeApp({
-  credential: cert(serviceAccount as any),
+  credential: cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  }),
 });
 
 export const db = getFirestore(firebaseApp);
